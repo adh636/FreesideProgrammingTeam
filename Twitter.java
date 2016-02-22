05809c6f4f59ed9dd1dee6b4b2394d51d7e42142
Adam <adh636@gmail.com>
2016-02-22 12:29:51 -0500

    add comments to Twitter.java

--- Twitter.java
+++ Twitter.java
@@ -8,31 +8,32 @@ public class Twitter {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner in = new Scanner(new File("TwitterInput.txt"));
-		TreeMap<String, Integer> hashTags = getHashTags(in);
+		TreeMap<String, Integer> hashTags = getHashTags(in); // TreeMap to hold HashTag and counts
 		printHashTags(hashTags);
 	}
 	
 	public static TreeMap<String, Integer> getHashTags(Scanner in) {
 		TreeMap<String, Integer> hashTags = new TreeMap<String, Integer>();
 		while (in.hasNextLine()) {
-			String[] current = in.nextLine().split(" ");
-			for (String s: current) {
+			String[] current = in.nextLine().split(" "); // splits String into Array of Strings separated by spaces
+			for (String s: current) { // iterates through each element of the Array
 				if (s.length() > 0) {
-					if (s.substring(0, 1).equals("#")) {
-						String lcString = s.substring(1).toLowerCase();
+					if (s.substring(0, 1).equals("#")) { // look at Strings that start with #
+						String lcString = s.substring(1).toLowerCase(); // get String after #
 						if (!hashTags.containsKey(lcString)) {
-							hashTags.put(lcString, 1);
+							hashTags.put(lcString, 1); // if String not in TreeMap, add it with value of time
 						}
 						else {
-							hashTags.put(lcString, hashTags.get(lcString) + 1);
+							hashTags.put(lcString, hashTags.get(lcString) + 1); // else increment value by 1
 						}
 					}
 				}
 			}
 		}
-		return hashTags;
+		return hashTags; // return TreeMap with all HashTags and values
 	}
 	
+	// print TreeMap key/value pairs in order
 	public static void printHashTags(TreeMap<String, Integer> hashTags) {		
 		for(Map.Entry<String,Integer> entry : hashTags.entrySet()) {
 		    String key = entry.getKey();
