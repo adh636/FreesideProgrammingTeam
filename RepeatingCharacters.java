cd023aeb69c357ef32910a080c37d61d701094d5
Adam <adh636@gmail.com>
2016-03-23 23:12:39 -0400

    add comments

--- RepeatingCharacters.java
+++ RepeatingCharacters.java
@@ -9,23 +9,30 @@ public class RepeatingCharacters {
 		repeatCharacters(in);
 	}
 	
+	// prints out a string with each character from the input repeat a specified number of times
 	public static void repeatCharacters(Scanner in) {
+
 		System.out.println(Integer.parseInt(in.nextLine()));
 		
 		while (in.hasNextLine()) {
+
 			String[] inputArray = in.nextLine().split(" ");
-			int setNum = Integer.parseInt(inputArray[0]);
-			int repeatNum = Integer.parseInt(inputArray[1]);
-			String s = inputArray[2];
+			int setNum = Integer.parseInt(inputArray[0]); // specifies the number of the input set
+			int repeatNum = Integer.parseInt(inputArray[1]); // specifies number of times each character will repeat
+			String s = inputArray[2]; // specifies String to be modified
 			
 			StringBuilder repeatedString = new StringBuilder();
+
+			// loops through each character in the String
 			for (int i = 0; i < s.length(); i++) {
 				String current = s.substring(i, i+1);
 				
+				// loops through the number of times the character needs to be repeated
 				for (int j = 0; j < repeatNum; j++) {
 					repeatedString.append(current);
 				}
 			}
+			
 			System.out.println(setNum + " " + repeatNum + " " + repeatedString.toString());
 		}
 	}
