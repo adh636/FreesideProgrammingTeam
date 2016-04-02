ee5a886210f295d23be59e5ed9e1c9dbbad8c5f7
Adam <adh636@gmail.com>
2016-04-02 19:51:07 -0400

    remove unnecessary if statement

--- Twitter.java
+++ Twitter.java
@@ -17,15 +17,13 @@ public class Twitter {
 		while (in.hasNextLine()) {
 			String[] current = in.nextLine().split(" "); // splits String into Array of Strings separated by spaces
 			for (String s: current) { // iterates through each element of the Array
-				if (s.length() > 0) {
-					if (s.substring(0, 1).equals("#")) { // look at Strings that start with #
-						String lcString = s.substring(1).toLowerCase(); // get String after #
-						if (!hashTags.containsKey(lcString)) {
-							hashTags.put(lcString, 1); // if String not in TreeMap, add it with value of time
-						}
-						else {
-							hashTags.put(lcString, hashTags.get(lcString) + 1); // else increment value by 1
-						}
+				if (s.substring(0, 1).equals("#")) { // look at Strings that start with #
+					String lcString = s.substring(1).toLowerCase(); // get String after #
+					if (!hashTags.containsKey(lcString)) {
+						hashTags.put(lcString, 1); // if String not in TreeMap, add it with value of time
+					}
+					else {
+						hashTags.put(lcString, hashTags.get(lcString) + 1); // else increment value by 1
 					}
 				}
 			}
